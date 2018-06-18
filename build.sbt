86dec76a87d9bccd5a003f5c789df5a46ef1bf74
László van den Hoek <laszlo.vandenhoek@gmail.com>
2018-06-18 16:08:24 +0200

    add local server to make example self-contained

--- build.sbt
+++ build.sbt
@@ -4,13 +4,13 @@ version := "0.1"
 
 scalaVersion := "2.11.12"
 
-val akkaVersion = "2.5.8"
+val akkaVersion = "2.5.13"
 
 libraryDependencies ++= Seq(
     "com.typesafe.akka" %% "akka-actor" % akkaVersion,
     "com.typesafe.akka" %% "akka-slf4j" % akkaVersion,
     "com.typesafe.akka" %% "akka-stream" % akkaVersion,
-    "com.typesafe.akka" %% "akka-http" % "10.1.0-RC1",
+    "com.typesafe.akka" %% "akka-http" % "10.1.3",
     "ch.qos.logback" % "logback-classic" % "1.1.7",
     "org.log4s" %% "log4s" % "1.3.0"
 )
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,30 +1,63 @@
-import scala.util.{Failure, Success}
-import scala.concurrent.duration._
-import scala.concurrent.ExecutionContext.Implicits.global
-import akka.actor.ActorSystem
-import akka.pattern.ask
 import akka.Done
-import akka.util.Timeout
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.common.{CsvEntityStreamingSupport, EntityStreamingSupport}
+import akka.http.scaladsl.marshalling.{Marshaller, Marshalling}
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.server.Directives._
+import akka.pattern.ask
+import akka.stream.scaladsl.Source
+import akka.stream.{ActorMaterializer, Materializer}
+import akka.util.{ByteString, Timeout}
 import com.typesafe.config.ConfigFactory
 
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
 object Main {
-    private val log = org.log4s.getLogger
+  private val log = org.log4s.getLogger
 
-    def main(args: Array[String]): Unit = {
-        val config = ConfigFactory.load()
-        val system = ActorSystem("downloader")
-        val downloader = system.actorOf(PullSupervisor.props(config, system, "http://localhost:8000/random_1m.csv"))
+  private def server(): Future[Http.ServerBinding] = {
+    implicit val system: ActorSystem = ActorSystem("server")
+    implicit val materializer: Materializer = ActorMaterializer()
+    implicit val marshaller: Marshaller[String, ByteString] =
+      Marshaller.strict[String, ByteString] { s =>
+        Marshalling.WithFixedContentType(ContentTypes.`text/csv(UTF-8)`, () => {
+          ByteString(s"$s," * 100 + "\r\n")
+        })
+      }
+    implicit val csvEntityStreamingSupport: CsvEntityStreamingSupport = EntityStreamingSupport.csv()
 
-        implicit val timeout = Timeout(5.minutes)
-
-        (downloader ? PullSupervisor.StartPull).mapTo[Done] onComplete {
-            case Success(Done) =>
-                log.info("Completed successfully")
-                system.terminate()
-
-            case Failure(e) =>
-                log.error(e)("Failed")
-                system.terminate()
-        }
+    val route = path(Remaining) { p =>
+      complete(
+        Source.repeat(p).take(1000000)
+      )
     }
+
+    Http().bindAndHandle(route, "localhost", 8000)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val config = ConfigFactory.load()
+    val system = ActorSystem("downloader")
+
+    server().foreach(binding => {
+
+      val downloader = system.actorOf(PullSupervisor.props(config, system, "http://localhost:8000/random_1m.csv"))
+
+      implicit val timeout = Timeout(5.minutes)
+
+      (downloader ? PullSupervisor.StartPull).mapTo[Done] onComplete {
+        case Success(Done) =>
+          log.info("Completed successfully")
+          system.terminate()
+
+        case Failure(e) =>
+          log.error(e)("Failed")
+          system.terminate()
+      }
+    })
+  }
 }
